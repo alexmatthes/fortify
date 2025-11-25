657a86cdd64e00acafa76e2235c6574920c2ae73
Alex Matthes <alharrisonmatthes8@gmail.com>
2025-11-25 00:28:48 -0500

    Implemented better logging tools and automated tests

---
+++ backend/utils/logger.ts
@@ -0,0 +1,25 @@
+import winston from 'winston';
+import { config } from '../config';
+
+const logger = winston.createLogger({
+	level: config.nodeEnv === 'development' ? 'debug' : 'info',
+	format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+	transports: [
+		// Write all logs with importance level of `error` or less to `error.log`
+		new winston.transports.File({ filename: 'error.log', level: 'error' }),
+		// Write all logs to `combined.log`
+		new winston.transports.File({ filename: 'combined.log' }),
+	],
+});
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+if (config.nodeEnv !== 'production') {
+	logger.add(
+		new winston.transports.Console({
+			format: winston.format.simple(),
+		})
+	);
+}
+
+export default logger;
